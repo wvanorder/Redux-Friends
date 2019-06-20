42d23ea1097f5af0a66f40851cd1112b3df2a3fa
VanOrder <vanorder@VanOrders-iMac.local>
2019-06-20 14:14:48 -0800

    mvp met

---
+++ friends/src/components/friendsList.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import Loader from 'react-loader-spinner';
+import { withRouter } from  'react-router-dom';
+
+import { fetchData, addFriend, deleteFriend } from '../actions';
+
+class FriendsList extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            newFriend: {
+                name: '',
+                age: '',
+                email: '',
+            }
+        }
+    }
+    componentDidMount() {
+        this.props.fetchData();
+    }
+
+    handleChange = e => {
+        e.preventDefault();
+        this.setState({
+                newFriend:{
+                ...this.state.newFriend,
+                [e.target.name]: e.target.value
+            }
+        })
+    }
+
+    addFriend = e => {
+        this.props.addFriend(this.state.newFriend);
+    }
+
+    deleteFriend = id => {
+        this.props.deleteFriend(id);
+    }
+
+    
+    render() {
+        return(
+            <div>
+                {this.props.friends.map(friend => {
+                    return (<div>
+                                <h1>{friend.name}</h1>
+                                <button onClick={() => this.deleteFriend(friend.id)}>Delete</button>
+                            </div>
+                    )
+              })}
+          <div className="form">
+            <form>
+                <input placeholder="Friend Name"
+                onChange={this.handleChange}
+                value={this.state.newFriend.name}
+                name="name"
+                required = 'Fill Me out'
+                />
+                <input placeholder ='email'
+                onChange={this.handleChange}
+                value={this.state.newFriend.email}
+                name="email"
+                required = 'Fill Me out'
+                />
+                <input placeholder ='age'
+                onChange={this.handleChange}
+                value={this.state.newFriend.age}
+                name="age"
+                required = 'Fill Me out'
+                />
+                {/* {props.error && <p>{props.error}</p>} */}
+            </form>
+            <button onClick={this.addFriend}>Add Friend</button>
+            {/* <button onClick={props.removeFriend}>Remove Friend</button> */}
+            
+        </div>
+            </div>
+        )
+    }
+};
+
+FriendsList.defaultProps = {
+    friends: []
+};
+
+const mapStateToProps = state => {
+    return{
+        friends: state.friends,
+        fetchingData: state.fetchingData
+    }
+};
+
+export default withRouter(
+    connect(
+        mapStateToProps,
+        { fetchData, addFriend, deleteFriend }
+    )(FriendsList)
+);
---
+++ friends/src/reducers/index.js
@@ -0,0 +1,67 @@
+import {
+    LOGIN_START,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE,
+    FETCH_DATA_START,
+    FETCH_DATA_SUCCESS,
+    FETCH_DATA_FAILURE,
+    ADD_FRIEND,
+    DELETE_FRIEND
+} from '../actions';
+
+const initialState = {
+    friends: [],
+    error: '',
+    fetchingData: false,
+    loggingIn: false,
+};
+
+const reducer = (state= initialState, action) => {
+    switch(action.type) {
+        case LOGIN_START:
+            return{
+                ...state,
+                error: '',
+                loggingIn: true,
+            };
+        case LOGIN_SUCCESS:
+            return{
+                ...state,
+                error: '',
+                loggingIn: false,
+            };
+        case FETCH_DATA_START:
+            return{
+                ...state,
+                error: '',
+                fetchingData: true,
+            };
+        case FETCH_DATA_SUCCESS:
+            return{
+                ...state,
+                fetchingData: false,
+                error: '',
+                friends: action.payload
+            };
+        case FETCH_DATA_FAILURE:
+            return{
+                ...state,
+                fetchingData: false,
+                error: action.payload
+            };
+            case ADD_FRIEND:
+            return{
+                ...state,
+                friends: action.payload
+            }
+            case DELETE_FRIEND:
+            return{
+                ...state,
+                friends: action.payload
+            }
+        default:
+        return state;
+    }
+};
+
+export default reducer;
